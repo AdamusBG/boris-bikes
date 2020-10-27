4c478e50b70082a797b8e53871662ae4fd81bd39
Adam G <adamwizzy@hotmail.co.uk>
2020-10-27 16:58:14 +0000

    completed up to end of exercise 13

--- lib/dockingstation.rb
+++ lib/dockingstation.rb
@@ -1,5 +1,29 @@
+require_relative './bike'
+
+
 class DockingStation
-  def release_bike
-    Bike.new
+  attr_reader :bike_array
+
+  def initialize
+  	@bike_array = []
   end
+
+
+  def release_bike
+  	if @bike_array.empty?
+  	  raise StandardError.new "There were no bikes in the docking station"
+  	else
+      Bike.new
+    end
+  end
+
+  def dock(bike)
+  	if @bike_array.empty?
+  	  @bike_array.push(bike)
+  	else
+  	  raise StandardError.new "You cannot dock a bike, this station already contains one"
+  	end
+  end
+
 end
+
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,10 +1,23 @@
 require 'dockingstation'
+require 'bike'
 
 describe 'DockingStation' do
   it { DockingStation.should respond_to(:new) }
   it { DockingStation.new.should respond_to(:release_bike) }
   it 'Releases a bike and then applies .working? method to released bike' do
-  	bike = DockingStation.new.release_bike
+  	station = DockingStation.new
+  	station.dock(Bike.new) # had to include this line and one above after feature was added to throw error when calling .release_bike if bike_array is empty
+  	bike = station.release_bike
   	expect(bike.working?).to eq true
+  end
+  it { DockingStation.new.should respond_to(:dock) }
+  it 'Throws an error when trying to release bike from empty docking station' do
+  	expect{DockingStation.new.release_bike}.to raise_error
+  end
+  it 'Throws an error when trying to add bike to docking station with bike already docked' do
+  	expect{
+  	  station = DockingStation.new
+  	  station.dock(Bike.new)
+  	  station.dock(Bike.new)}.to raise_error
   end
 end
